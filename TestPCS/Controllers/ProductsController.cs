f73a776ffa54865cc9733b62a61b449708ffc334
Jorge <jegnzc@gmail.com>
2024-03-18 00:44:21 -0600

    Integrate ProductService into ProductsController

--- TestPCS/Controllers/ProductsController.cs
+++ TestPCS/Controllers/ProductsController.cs
@@ -1,9 +1,50 @@
+using Application.Interfaces;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TestPCS.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ProductsController : ControllerBase
+public class ProductsController(IProductService productService) : ControllerBase
 {
+    private readonly IProductService _productService = productService;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+    {
+        var products = await _productService.GetAllProductsAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProductById(string id)
+    {
+        var product = await _productService.GetProductByIdAsync(id);
+        if (product == null)
+            return NotFound();
+
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateProduct([FromBody] Product product)
+    {
+        await _productService.CreateProductAsync(product);
+        return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateProduct(string id, [FromBody] Product product)
+    {
+        await _productService.UpdateProductAsync(id, product);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteProduct(string id)
+    {
+        await _productService.DeleteProductAsync(id);
+        return NoContent();
+    }
 }
